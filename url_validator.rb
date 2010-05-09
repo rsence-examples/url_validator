9f27ac0f7c3a9babc9e662320f071e75a801a3f0
Toni Nurminen <tznurmin@80-248-246-103.cust.suomicom.fi>
2010-05-10 01:11:40 +0300

    Real time URL validation example

---
+++ url_validator.rb
@@ -0,0 +1,42 @@
+class URLValidator < GUIPlugin
+require 'resolv'
+require 'uri'
+require 'net/http'
+  
+  def open
+    super
+    puts "opened!"
+    begin
+      @dns = Resolv::DNS.new
+    rescue
+      puts "#{e.inspect}"
+    end
+  end
+  
+
+  def domain_valid?( msg, url_string )
+    puts "checking domain validity... #{url_string.data}"
+    begin
+      domain = URI.parse(url_string.data).host
+    rescue
+      puts "shazbot! something funky happened.."
+      set_state( msg, false )
+      return true
+    end
+    puts "URI formed"
+    if domain == nil
+      set_state( msg, false )
+      return true
+    end
+    if (domain[0..3] == "www.") 
+      domain = domain[4..domain.length] 
+    end
+    set_state( msg, @dns.getresources(domain, Resolv::DNS::Resource::IN::A).size > 0 )
+  return true
+  end
+  
+  def set_state( msg, state )
+    ses = get_ses( msg )
+    ses[:valid_state].set( msg, state )
+  end
+end
